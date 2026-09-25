9f0fa9bbb7752d19631cda692c0273bdd537fb80
Haze10425 <jheee042500@gmail.com>
2026-09-25 17:45:11 +0900

    feat: 로그인 API 구현

--- routes/auth.js
+++ routes/auth.js
@@ -16,15 +16,15 @@ router.post("/signup", async (req, res) => {
   if (username.length < 2 || username.length > 50) {
     return res.status(400).json({
       message: "이름은 2~50자로 입력해주세요.",
-    })
+    });
   }
 
   if (nickname.length < 2 || username.length > 50) {
     return res.status(400).json({
       message: "닉네임은 2~50자로 입력해주세요.",
-    })
+    });
   }
-  
+
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(email)) {
     return res.status(400).json({
@@ -35,7 +35,7 @@ router.post("/signup", async (req, res) => {
   if (password.length < 8 || password.length > 255) {
     return res.status(400).json({
       message: "비밀번호는 8~255자로 입력해주세요.",
-    })
+    });
   }
 
   try {
@@ -86,4 +86,58 @@ router.post("/signup", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "이메일과 비밀번호를 입력해주세요.",
+    });
+  }
+
+  db.query(
+    "SELECT id, username, nickname, email, password, status FROM users WHERE email = ?",
+    [email],
+    async (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({
+          message: "db 오류",
+        });
+      }
+
+      if (results.length === 0) {
+        return res.status(401).json({
+          message: "이메일 혹은 비밀번호가 올바르지 않습니다.",
+        });
+      }
+
+      const user = results[0];
+
+      if (user.status !== "active") {
+        return res.status(403).json({
+          message: "이용할 수 없는 계정입니다.",
+        });
+      }
+
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordMatch) {
+        return res.status(401).json({
+          message: "이메일 또는 비밀번호가 올바르지 않습니다.",
+        });
+      }
+
+      return res.status(200).json({
+        message: "로그인 성공",
+        user: {
+          id: user.id,
+          username: user.username,
+          nickname: user.nickname,
+          email: user.email,
+        },
+      });
+    }
+  );
+});
 module.exports = router;
